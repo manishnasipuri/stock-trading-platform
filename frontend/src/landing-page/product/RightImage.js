fe64f673c0b0ef5d7a1a8c461e4f5067d664e213
Manish Nasipuri <your_email@example.com>
2026-07-25 02:01:42 +0530

    Fix RightImage alt attribute

--- frontend/src/landing-page/product/RightImage.js
+++ frontend/src/landing-page/product/RightImage.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React from "react";
 
-function RightImage({imageURL,productName,productDescription,learnMore}) {
-    return ( 
-        <div className='container ' >
-            <div className='row' >
-                <div className='col-6 p-5 mt-5'>
-                    <h1 className='mt-5'>{productName}</h1>
-                    <p>{productDescription}</p>
-                    <a href={learnMore}> Learn More</a>
-                </div>
-                <div className='col-6'>
-                    <img src={imageURL}></img>
-                </div>
-            </div>
+function RightImage({
+  imageURL,
+  productName,
+  productDescription,
+  learnMore,
+}) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6 p-5 mt-5">
+          <h1 className="mt-5">{productName}</h1>
+          <p>{productDescription}</p>
+          <a href={learnMore}>Learn More</a>
         </div>
-     );
+
+        <div className="col-6">
+          <img
+            src={imageURL}
+            alt={productName}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RightImage;
