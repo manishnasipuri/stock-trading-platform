a54fe89f22d48ecbd91bf5b5545ca8555acd947d
Manish Nasipuri <your_email@example.com>
2026-07-25 02:04:56 +0530

    Fix ProductPage imports

--- frontend/src/landing-page/product/ProductPage.js
+++ frontend/src/landing-page/product/ProductPage.js
@@ -1,27 +1,72 @@
-import React from 'react';
-import Hero from './Hero';
-import Universe from './Universe';
-import LeftImage from './LeftImage';
-import RightImage from './RightImage';
-import Footer from '../Footer';
-import Navbar from '../Navbar';
+import React from "react";
+import Hero from "./Hero";
+import Universe from "./Universe";
+import LeftImage from "./LeftImage";
+import RightImage from "./RightImage";
+import Footer from "../Footer";
+import Navbar from "../Navbar";
 
 function ProductPage() {
-    return ( 
-        <>
-        
-        <Hero />
-        <LeftImage  imageURL= "media/kite.png"  productName= "Kite" productDescription= "Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices." tryDemo= "" learnPlay= "" googlePlay= "" appStore= ""  />
-        <RightImage imageURL= "media/console.png" productName= "Console" productDescription= "The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations." learnMore= ""  />
-        <LeftImage  imageURL= "media/coin.png"  productName= "Coin" productDescription= "Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices." tryDemo= "" learnPlay= "" googlePlay= "" appStore= ""  />
-         <RightImage imageURL= "media/kiteconnect.png" productName= "Kite Connect API" productDescription= "Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase." learnMore= ""  />
-        <LeftImage  imageURL= "media/varsity.png"  productName= "Varsity mobile" productDescription= "An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go." tryDemo= "" learnPlay= "" googlePlay= "" appStore= ""  />
+  return (
+    <>
+      <Navbar />
 
-        <p className='text-center mb-5'> Want to know more about our technology stack? Check out the Zerodha.tech blog.</p>
-        <Universe />
-        
-        </>
-     );
+      <Hero />
+
+      <LeftImage
+        imageURL="media/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnPlay=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <RightImage
+        imageURL="media/console.png"
+        productName="Console"
+        productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        learnMore=""
+      />
+
+      <LeftImage
+        imageURL="media/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnPlay=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <RightImage
+        imageURL="media/kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        learnMore=""
+      />
+
+      <LeftImage
+        imageURL="media/varsity.png"
+        productName="Varsity Mobile"
+        productDescription="An easy to grasp collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnPlay=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <p className="text-center mb-5">
+        Want to know more about our technology stack? Check out the Zerodha.tech
+        blog.
+      </p>
+
+      <Universe />
+
+      <Footer />
+    </>
+  );
 }
 
 export default ProductPage;
