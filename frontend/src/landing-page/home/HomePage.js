e00ce119511d77e5d9f431cd2bab0a50c5efce42
Manish Nasipuri <your_email@example.com>
2026-07-25 02:45:45 +0530

    Fix duplicate navbar and hero image

--- frontend/src/landing-page/home/HomePage.js
+++ frontend/src/landing-page/home/HomePage.js
@@ -5,22 +5,16 @@ import Stats from "./Stats";
 import Pricing from "./Pricing";
 import Education from "./Education";
 import OpenAccount from "../OpenAcc";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
 
 function HomePage() {
   return (
     <>
-      <Navbar />
-
       <Hero />
       <Awards />
       <Stats />
       <Pricing />
       <Education />
       <OpenAccount />
-
-      <Footer />
     </>
   );
 }
