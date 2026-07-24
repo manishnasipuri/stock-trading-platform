a9cd308ae84cfee55e78b04f3a2e260fef1f979e
Manish Nasipuri <your_email@example.com>
2026-07-25 02:07:45 +0530

    Fix LeftImage accessibility

--- frontend/src/landing-page/product/LeftImage.js
+++ frontend/src/landing-page/product/LeftImage.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React from "react";
 
-function LeftImage({imageURL,productName,productDescription,tryDemo,learnPlay,googlePlay,appStore}) {
-    return ( 
-       <div className='container mt-5'>
-            <div className='row'>
-                <div className='col-6 p-5 mb-5'>
-                    <img src={imageURL}></img>
-                </div>
-                <div className='col-6 p-5 mt-5'>
-                    <h1> {productName}</h1>
-                    <p>{productDescription}</p>
-                    <div>
-                        <a href={tryDemo}>Try Demo</a>
-                        <a href={learnPlay} style={{marginLeft:"120px"}}>learn Play</a>
-                    </div>
-                    <div className='mt-3'>
-                        <a href={googlePlay}><img src='media/googlePlayBadge.svg'></img></a>
-                        <a href={appStore} style={{marginLeft:"50px"}}><img src='media/appStoreBadge.svg'></img></a>
-                    </div>
-                    
-                </div>
-            </div>
-       </div>
-     );
+function LeftImage({
+  imageURL,
+  productName,
+  productDescription,
+  tryDemo,
+  learnPlay,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5 mb-5">
+          <img src={imageURL} alt={productName} />
+        </div>
+
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+
+          <div>
+            <a href={tryDemo}>Try Demo</a>
+            <a href={learnPlay} style={{ marginLeft: "120px" }}>
+              Learn More
+            </a>
+          </div>
+
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img
+                src="media/googlePlayBadge.svg"
+                alt="Get it on Google Play"
+              />
+            </a>
+
+            <a href={appStore} style={{ marginLeft: "50px" }}>
+              <img
+                src="media/appStoreBadge.svg"
+                alt="Download on the App Store"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftImage;
